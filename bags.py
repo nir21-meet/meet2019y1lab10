103c54e4ea33ac3c5beed21ed368fc2017ccdd00
nir21-meet <nir21@meet.mit.edu>
2019-08-04 10:55:33 -0400

    Stopping here

---
+++ bags.py
@@ -0,0 +1,24 @@
+import turtle
+import random
+bag_list=[]
+bag_pos=[]
+bag=turtle.clone()
+def make_bag():
+    bags=bag.stamp()
+    line=random.randint(1,3)
+    if line==1:
+        bag.goto(seaturtle.xcor(),seaturtle.ycore()+30)        
+        bag.stamp
+    if line==2:
+        bag.goto(seaturtle.xcor(),seaturtle.ycore())        
+        bag.stamp()
+    if line==3:
+        bag.goto(seaturtle.xcor(),seaturtle.ycore()-30)        
+        bag.stamp()
+    bagcor=bag.pos()
+    
+    pos.append(foodcor)
+    stamp1=food.stamp()
+    food_stamps.append(stamp1)
+    stamp=snake.stamp()
+    stamp_list.append(stamp)
